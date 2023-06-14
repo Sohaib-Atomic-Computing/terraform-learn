1ec55d867a2160f94234de50e99d827b2d1ce15f
Sohaib <sohaib.ismail@atomic-computing.com>
2023-06-14 19:51:10 +0500

    created vpc, subnet, routetable, igw, sg

--- main.tf
+++ main.tf
@@ -1,65 +1,128 @@
 provider "aws" {
-  region     = "eu-central-1"
+  region = "eu-central-1"
 }
 
-variable "cidr_blocks" {
-  description = "cidr blocks"
-  # type  = list(string)
-  type = list(object({
-    cidr_block = string
-    name       = string
-  }))
-}
+# variable "cidr_blocks" {
+#   description = "cidr blocks"
+#   # type  = list(string)
+#   type = list(object({
+#     cidr_block = string
+#     name       = string
+#   }))
+# }
 
-variable "vpc_cidr_block" {
-  description = "vpc/subnet cidr blocks and name tags for vpc/subnet "
-  default     = "10.0.0.0/16"
-  type        = string
-}
+# variable "vpc_cidr_block" {
+#   description = "vpc/subnet cidr blocks and name tags for vpc/subnet "
+#   default     = "10.0.0.0/16"
+#   type        = string
+# }
 
-resource "aws_vpc" "development-vpc" {
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+
+
+resource "aws_vpc" "myapp-vpc" {
   # cidr_block = var.cidr_blocks[0]   //this is used to access variable from a list of strings
-  cidr_block = var.cidr_blocks[0].cidr_block //this is used to access variable from a list of objects
-
-
+  # cidr_block = var.cidr_blocks[0].cidr_block //this is used to access variable from a list of objects
+  cidr_block = var.vpc_cidr_block
   tags = {
     # Name : "development"   
-    Name = var.cidr_blocks[0].name //this is used to access variable from a list of objects
+    # Name = var.cidr_blocks[0].name //this is used to access variable from a list of objects
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id = aws_vpc.development-vpc.id
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
   # cidr_block        = var.cidr_blocks[1]
-  cidr_block        = var.cidr_blocks[1].cidr_block
-  availability_zone = "eu-central-1a"
+  # cidr_block        = var.cidr_blocks[1].cidr_block
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.avail_zone
   tags = {
     # Name : "subnet-1-dev"
-    Name = var.cidr_blocks[1].name
+    # Name = var.cidr_blocks[1].name
+    Name = "${var.env_prefix}-subnet-1"
   }
 }
 
-data "aws_vpc" "existing_vpc" {
-  default = true
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
 }
 
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id            = data.aws_vpc.existing_vpc.id
-  cidr_block        = "172.31.48.0/20"
-  availability_zone = "eu-central-1a"
+# resource "aws_route_table" "myapp-route-table" {
+#   vpc_id = aws_vpc.myapp-vpc.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.myapp-igw.id
+#   }
+#   tags = {
+#     Name = "${var.env_prefix}-rtb"
+#   }
+# }
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
   tags = {
-    Name : "subnet-2-default"
+    Name = "${var.env_prefix}-main-rtb"
+  }
+}
+
+# resource "aws_route_table_association" "a-rtb-subnet" {
+#   subnet_id      = aws_subnet.myapp-subnet-1.id
+#   route_table_id = aws_route_table.myapp-route-table.id
+# }
+
+# data "aws_vpc" "existing_vpc" {
+#   default = true
+# }
+
+resource "aws_default_security_group" "default-sg" {
+  # name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port  = 22
+    to_port    = 22
+    protocol   = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port  = 8080
+    to_port    = 8080
+    protocol   = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
   }
 }
 
 output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
+  value = aws_vpc.myapp-vpc.id
 }
 
 output "dev-vpc-arn" {
-  value = aws_vpc.development-vpc.arn
+  value = aws_vpc.myapp-vpc.arn
 }
 
 output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
+  value = aws_subnet.myapp-subnet-1.id
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,8 +1,13 @@
-cidr_blocks = [{ cidr_block = "10.0.0.0/16", name = "dev-vpc" },
-{ cidr_block = "10.0.10.0/24", name = "dev-subnet" }]
+# cidr_blocks = [{ cidr_block = "10.0.0.0/16", name = "dev-vpc" },
+# { cidr_block = "10.0.10.0/24", name = "dev-subnet" }]
 
 # cidr_blocks       = ["10.0.0.0/16", "10.0.10.0/24"]
 
 # subnet_cidr_block = "10.0.10.0/24"
 # vpc_cidr_block    = "10.0.0.0/16"
 
+
+vpc_cidr_block = "10.0.0.0/16"
+subnet_cidr_block = "10.0.10.0/24"
+avail_zone = "eu-central-1a"
+env_prefix = "dev"
