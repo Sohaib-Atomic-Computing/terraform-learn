e954e9b346ca416b8b182acb44006e5dae364f69
Sohaib <sohaib.ismail@atomic-computing.com>
2023-06-15 00:21:06 +0500

    created and configured ec2 instance

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
+variable "instance_type" {}
 
 
 resource "aws_vpc" "myapp-vpc" {
@@ -82,31 +83,28 @@ resource "aws_default_route_table" "main-rtb" {
 #   route_table_id = aws_route_table.myapp-route-table.id
 # }
 
-# data "aws_vpc" "existing_vpc" {
-#   default = true
-# }
 
 resource "aws_default_security_group" "default-sg" {
   # name   = "myapp-sg"
   vpc_id = aws_vpc.myapp-vpc.id
 
   ingress {
-    from_port  = 22
-    to_port    = 22
-    protocol   = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port  = 8080
-    to_port    = 8080
-    protocol   = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks     = ["0.0.0.0/0"]
     prefix_list_ids = []
   }
 
@@ -115,14 +113,43 @@ resource "aws_default_security_group" "default-sg" {
   }
 }
 
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   owners      = ["amazon"]
+#   filter {
+#     name   = "name"
+#     values = ["Ubuntu Server * Type"]
+#   }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
+
+# output "aws_ami_id" {
+#   value = data.aws_ami.ubuntu.id
+# }
+
+# resource "aws_key_pair" "ssh-key" {
+#   key_name = "server-key"
+
+# }
+
+resource "aws_instance" "myapp-server" {
+  ami = "ami-09db9d79b41ec058e"
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+  key_name = "Frankfurt-Keypair"
+   tags = {
+    Name = "${var.env_prefix}-server"
+  }
+  
+}
+
 output "dev-vpc-id" {
   value = aws_vpc.myapp-vpc.id
 }
 
-output "dev-vpc-arn" {
-  value = aws_vpc.myapp-vpc.arn
-}
-
-output "dev-subnet-id" {
-  value = aws_subnet.myapp-subnet-1.id
-}
