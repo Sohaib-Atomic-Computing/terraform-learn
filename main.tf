84a654b5a6ba2c28486420fcef1c3f7283a2996b
Sohaib <sohaib.ismail@atomic-computing.com>
2023-06-21 16:46:07 +0500

    added remote state file

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">=1.4.2"
+  backend "s3" {
+    bucket = "myapp-buckettt"
+    key = "myapp/state.tfstate"
+    region = "eu-central-1"
+  }
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
